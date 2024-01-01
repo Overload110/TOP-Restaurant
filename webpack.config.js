ee73832922adbe231445956f1f2358ea20375904
Overload110 <149197288+Overload110@users.noreply.github.com>
2024-01-01 12:23:05 -0600

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     output: {
       filename: 'main.js',
       path: __dirname + '/dist'
-    }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+        ]
+      }
   };
